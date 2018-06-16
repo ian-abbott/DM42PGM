d2acd3cfa3c6be1c0846a59b7e329a0a0579c0bf
Ian Abbott <ian@abbott.org>
2018-06-16 18:48:42 +0100

    Remove semicolon from beginning of STATIC_ASSERT expansion

--- dmcp/dmcp.h
+++ dmcp/dmcp.h
@@ -343,7 +343,7 @@ typedef struct {
 // STATIC_ASSERT ...
 #define ASSERT_CONCAT_(a, b) a##b
 #define ASSERT_CONCAT(a, b) ASSERT_CONCAT_(a, b)
-#define STATIC_ASSERT(e,m) ;enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(int)(!!(e)) }
+#define STATIC_ASSERT(e,m) enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(int)(!!(e)) }
 //#define STATIC_ASSERT(expr, msg) typedef char ASSERT_CONCAT(static_assert_check_, __LINE__) [(expr) ? (+1) : (-1)]
 
 #define _STRINGIFY(x) #x
